82b8c7961910052d9a500188e0b31cf5c8b27635
Rakibul Hassan Rony <rakibul.hassan@dsinnovators.com>
2022-03-23 14:28:28 +0600

    reading input

--- hello.pl
+++ hello.pl
@@ -3,6 +3,6 @@ use strict;
 use warnings; 
 print "Hello World! with Perl\n";
 print "Enter your name: ";
-my $name = <>;
+my $name = <>; # to read input my keyword is used in perl
 chomp $name; # to del the new line char from the variable
 print "Welcome $name in perl learning journey"
