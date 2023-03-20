565f6ac4c8f7e4a2a8481a826328dd95760d8c17
Rakibul Hassan Rony <rakibulhassan.rony123@gmail.com>
2023-03-20 13:54:44 +0000

    3/20/2023, 7:54 PM

--- array.pl
+++ array.pl
@@ -1,25 +1,29 @@
-#!/usr/bin/perl
+
 use 5.010;
 use strict;
 use warnings;
 
-my @months = qw/Jan Feb Mar Apr May Jun Jul Aug Sept Oct Nov Dec/;
+# my @months = qw/Jan Feb Mar Apr May Jun Jul Aug Sept Oct Nov Dec/;
 
 
-print "$months[0]\n";
-print "$months[1]\n";
-print "$months[7]\n";
-print "$months[9]\n";
-print "$months[6]\n";
-print "$months[-3]\n"
+# print "$months[0]\n";
+# print "$months[1]\n";
+# print "$months[7]\n";
+# print "$months[9]\n";
+# print "$months[6]\n";
+# print "$months[-3]\n"
 
 
-@array = (you, me, us);
+my @array = ("you", "me", "us");
 $array[5] = 4;
-$size = @array;
-$index_max = $#array;
+my $size = @array;
+my $index_max = $#array;
 print "Size:  $size\n";
 print "Maximum Index: $index_max\n";
+
+for(my $i=0; $i<$index_max; $i++){
+    print "$i: $array[$i]\n";
+}
 
 
 
