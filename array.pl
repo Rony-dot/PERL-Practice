6cdc05014af9f67902a4cef17416c3b062596b59
Rakibul Hassan Rony <rakibulhassan.rony123@gmail.com>
2023-03-20 13:12:11 +0000

    3/20/2023, 7:12 PM

--- array.pl
+++ array.pl
@@ -3,21 +3,21 @@ use 5.010;
 use strict;
 use warnings;
 
-# my @months = qw/Jan Feb Mar Apr May Jun Jul Aug Sept Oct Nov Dec/;
+my @months = qw/Jan Feb Mar Apr May Jun Jul Aug Sept Oct Nov Dec/;
 
 
-# print "$months[0]\n";
-# print "$months[1]\n";
-# print "$months[7]\n";
-# print "$months[9]\n";
-# print "$months[6]\n";
-# print "$months[-3]\n"
+print "$months[0]\n";
+print "$months[1]\n";
+print "$months[7]\n";
+print "$months[9]\n";
+print "$months[6]\n";
+print "$months[-3]\n"
 
 
-my @array = ("you", "me", "us");
-my $array[5] = 4;
-my $size = @array;
-my $index_max = $#array;
+@array = (you, me, us);
+$array[5] = 4;
+$size = @array;
+$index_max = $#array;
 print "Size:  $size\n";
 print "Maximum Index: $index_max\n";
 
