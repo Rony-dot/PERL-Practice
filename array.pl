6676f072d54f4024d5f13617509413b3c958251b
Rakibul Hassan Rony <rakibulhassan.rony123@gmail.com>
2023-03-20 20:00:02 +0600

    for loop, and if else

--- array.pl
+++ array.pl
@@ -3,15 +3,15 @@ use 5.010;
 use strict;
 use warnings;
 
-# my @months = qw/Jan Feb Mar Apr May Jun Jul Aug Sept Oct Nov Dec/;
+my @months = qw/Jan Feb Mar Apr May Jun Jul Aug Sept Oct Nov Dec/;
 
 
-# print "$months[0]\n";
-# print "$months[1]\n";
-# print "$months[7]\n";
-# print "$months[9]\n";
-# print "$months[6]\n";
-# print "$months[-3]\n"
+print "$months[0]\n";
+print "$months[1]\n";
+print "$months[7]\n";
+print "$months[9]\n";
+print "$months[6]\n";
+print "$months[-3]\n";
 
 
 my @array = ("you", "me", "us");
@@ -21,8 +21,9 @@ my $index_max = $#array;
 print "Size:  $size\n";
 print "Maximum Index: $index_max\n";
 
-for(my $i=0; $i<$index_max; $i++){
-    print "$i: $array[$i]\n";
+for(my $i=0; $i<=$index_max; $i++){
+    if(defined $array[$i]) {print "$i: $array[$i]\n";}
+    else {print "kochu\n"};
 }
 
 
