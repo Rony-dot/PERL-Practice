4b9d0b0f45bbcce05c04a7c45c65ff1885c69cff
Rakibul Hassan Rony <rakibulhassan.rony123@gmail.com>
2023-03-20 11:55:05 +0000

    3/20/2023, 5:55 PM

---
+++ array.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+use 5.010;
+use strict;
+use warnings;
+
+my @months = qw/Jan Feb Mar Apr May Jun Jul Aug Sept Oct Nov Dec/;
+
+
+print "$months[0]\n";
+print "$months[1]\n";
+print "$months[7]\n";
+print "$months[9]\n";
+print "$months[6]\n";
+print "$months[-3]\n"
