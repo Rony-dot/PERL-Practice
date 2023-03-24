a75e2d05f76c7706be23dc8be3696d69a067de99
Rakibul Hassan Rony <rakibulhassan.rony123@gmail.com>
2023-03-24 12:09:09 +0000

    3/24/2023, 6:09 PM

--- variables.pl
+++ variables.pl
@@ -11,3 +11,21 @@ my $my_info = "$name lives on \"$addr\"\n";
 print $my_info;
 $my_info = qq{$name lives on "$addr"\n};
 print $my_info;
+
+my $bunch_of_info = <<"END";
+this is a
+multi line
+input for perl
+END
+say $bunch_of_info;
+
+my $big_int = 18446744073709551615;
+# %c : Character
+# %s : String
+# %d : Decimal
+# %u : Unsigned Integer
+# %f : Floating Point (Decimal Notation)
+# %e : Floation Point (Scientific Notation)
+printf("%u \n",$big_int+1);
+
+my $big_float - .1000000000000001;
